ab6151f7a05f426b5e43c9d21fa9845c3ab3d88a
Evgeny Savonkin <evgenysavonkin@gmail.com>
2024-01-12 22:35:21 +0300

    Create prototype of the main func (antiplagiarism)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
+double antiplagiarism(string text, string fragment);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+double antiplagiarism(string text, string fragment) {
+    double rate = 0.0;
+
+    //do some code
+
+    return rate;
+}
+
